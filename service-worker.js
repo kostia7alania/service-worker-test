1ec7c24e5dc8f2a62694d02227d461f12d9c5cda
Базров Константин Валерьевич <Konstantin.Bazrov@moex.com>
2019-12-10 16:53:45 +0300

    WIP

--- service-worker.js
+++ service-worker.js
@@ -1,94 +1,106 @@
-const CACHE_NAME = 'cache-articles-v1';
 const CACHE_VERSION = '6.2.1';
 const DOMAIN = location.origin;
 
 const CURRENT_CACHES = {
-  profile: `profile-cache-v-${CACHE_VERSION}`,
-  font: `font-cache-v-${CACHE_VERSION}`
+    core: {
+        name: `core-cache-v-${CACHE_VERSION}`,
+        path: null,
+    },
+    js: {
+        name: `js-cache-v-${CACHE_VERSION}`,
+        path: /\/_nuxt\/.*\.js/,
+    },
+    css: {
+        name: `css-cache-v-${CACHE_VERSION}`,
+        path: /\/_nuxt\/.*\.css/,
+    },
+    other: {
+        name: `other-cache-v-${CACHE_VERSION}`,
+        path: /\/ _nuxt\/.*/,
+    }
 };
 
 const BLACK_LIST = [
-  '__webpack_hmr'
+    '__webpack_hmr'
 ];
 
 const fallbackImage = '/img/pwa/fallback/offline.svg';
 
 const resourcesToPrecache = [
-  '/_nuxt/app.js',
-  '/_nuxt/vendors.app.js',
-  '/_nuxt/commons.app.js',
-  fallbackImage,
+    fallbackImage,
 ];
 
 const onInstall = event => {
-  console.log('[SW] install', event);
-  event.waitUntil( // install происходит перед тем, как cache готов, ФИКС - waitUntil
-    caches.open(CACHE_NAME) // октрываем кеш с именем cacheName
-      .then(cache => {
-        // cache.addAll([‘page2.html’]); // добавляем в кеш необязательные ресурсы
-        return cache.addAll(resourcesToPrecache) // добавляем в кеш обязательные ресурсы
-      })
-  )
+    console.log('[SW] install', event);
+    event.waitUntil( // install происходит перед тем, как cache готов, ФИКС - waitUntil
+        caches.open(CURRENT_CACHES.core) // октрываем кеш с именем cacheName
+            .then(cache => {
+                // cache.addAll([‘page2.html’]); // добавляем в кеш необязательные ресурсы
+                return cache.addAll(resourcesToPrecache) // добавляем в кеш обязательные ресурсы
+            })
+    )
 }
 
 // Хук activated - идеально подходит для удаления из кеша, он запускается, когда код SW изменен.
 const onActivate = event => {
-  console.log('[SW] activate', event);
-  const expectedCacheNames = Object.values(CURRENT_CACHES)
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => { // условия удаления
-            if (!expectedCacheNames.includes(cacheName)) {
-              console.log('[SW] deleting obsolete cache... =>', cacheName)
-              return caches.delete(cacheName)
-            }
-          })
-        )
-      })
-  )
+    console.log('[SW] activate', event);
+    const expectedCacheNames = CURRENT_CACHES.map(e => e.name)
+    event.waitUntil(// passing a Promise to extend the activating stage until the promise is resolved.
+        caches.keys()
+            .then(cacheNames => Promise.all(
+                cacheNames.map(cacheName => { // условия удаления
+                    if (!expectedCacheNames.includes(cacheName)) {
+                        console.log('[SW] deleting obsolete cache... =>', cacheName)
+                        return caches.delete(cacheName)
+                    }
+                })
+            ))
+    )
+    // self.ClientRectList.claim() // start controlling all open clients without reloading them
 }
 
 const onFetch = event => {
-  //caches.match(event.request).then(res => res || fetch(e.request)) // CACHE FIRST
-  const request = event.request
+    //caches.match(event.request).then(res => res || fetch(e.request)) // CACHE FIRST
+    const request = event.request
 
-  if ( request.method !== 'GET' ||
-      !request.url.match(DOMAIN) ||
-      BLACK_LIST.findIndex(item => request.url.match(item)) !== -1 ) {
-    return console.warn('[SW] fetch skipped => ', request)
-  }
+    if (request.method !== 'GET' ||
+        !request.url.match(DOMAIN) ||
+        BLACK_LIST.findIndex(item => request.url.match(item)) !== -1) {
+        return console.warn('[SW] fetch skipped => ', request)
+    }
+
+    const currentCacheObj = Object.values(CURRENT_CACHES).find(curCache => request.url.match(curCache.path)) || CURRENT_CACHES.other
+    const currentCacheName = currentCacheObj.name
+
     event.respondWith(
-      caches.open(CACHE_NAME)
-        .then(cache => {
-          return cache.match(request)
-            .then(res => {
-              const fetchPromise = () => fetch(request)
-                .then(networkResponse => {
-                  console.log('[SW] cache.put =>', request, networkResponse)
-                  cache.put(request, networkResponse.clone())
-                  return networkResponse
-                })
-              if (res) {
-                console.warn('[sw] fetching from SW.... ')
-                return res
-              }
-              return fetchPromise() // STRATEGY - Stale While Revalidate
+        caches.open(currentCacheName)
+            .then(cache => {
+                return cache.match(request)
+                    .then(res => {
+                        if (res) {
+                            console.warn('[sw] fetching from SW.... ')
+                            return res
+                        }
+                        return fetch(request) // STRATEGY - Stale While Revalidate
+                            .then(networkResponse => {
+                                console.log('[SW] cache.put =>', request, networkResponse)
+                                cache.put(request, networkResponse.clone())
+                                return networkResponse
+                            })
+                    })
             })
-        })
-        .catch(error => {
-          if (request.headers.get("Accept").includes("image")) { // fallback for images
-            return caches.match(fallbackImage)
-          }
-        })
+            .catch(error => {
+                if (request.headers.get("Accept").includes("image")) { // fallback for images
+                    return caches.match(fallbackImage)
+                }
+            })
     )
 }
 
 
-const onDownload  = event => console. log('[SW] download =>',  event);
-const onPush      = event => console. log('[SW] push =>',      event);
-const onSync      = event => console. log('[SW] sync =>',      event);
+const onDownload = event => console.log('[SW] download =>', event);
+const onPush = event => console.log('[SW] push =>', event);
+const onSync = event => console.log('[SW] sync =>', event);
 const onRedundant = event => console.warn('[SW] redundant =>', event)
 
 self.addEventListener('install', onInstall);
